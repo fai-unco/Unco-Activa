7dc76177005f221d8ca0c14b33221b39e08babb3
cristian96-code <cristian.garrado@est.fi.uncoma.edu.ar>
2022-08-13 02:19:53 -0300

    fixed footer name

--- frontend/src/components/auth/RegisterForm.jsx
+++ frontend/src/components/auth/RegisterForm.jsx
@@ -33,7 +33,7 @@ const RegisterForm = () => {
     return (
 
         <div className="grid items-center justify-center py-40 min-h-screen overflow-hidden">
-            <div className="w-auto md:w-2/3 p-6 m-auto bg-neutral-100 rounded-md shadow-md lg:max-w-xl">
+            <div className="w-auto p-6 m-auto bg-neutral-100 rounded-md shadow-md lg:max-w-xl">
                 <img className="center " src="/UNCO_Activa.svg"></img>
                 <h1 className="text-sm font-semibold text-center mt-10 text-gray-darker">
                     Registrate
@@ -45,12 +45,43 @@ const RegisterForm = () => {
                         type="text"
                         holder="Ingresa tu Nombre" 
                         divclass="mb-3" 
-                  />
-                    <InputForm value={surname} onChange={(e)=>setSurname(e.target.value)} type="text" holder="Ingresa tu Apellido" divclass="mb-3" />
-                    <InputForm value={dni} onChange={(e)=>setDni(e.target.value)} type="number" holder="Ingresa tu DNI" divclass="mb-3" />
-                    <InputForm value={phone} onChange={(e)=>setPhone(e.target.value)} type="tel" holder="Ingresa tu telefono" divclass="mb-3" />
-                    <InputForm value={email} onChange={(e)=>setEmail(e.target.value)} type="email" holder="Ingresa tu Email" divclass="mb-3" />
-                    <InputForm value={password} onChange={(e)=>setPassword(e.target.value)} type="password" holder="Ingresa una contraseña" divclass="mb-3 "/>
+                />
+                <InputForm 
+                    value={surname} 
+                    onChange={(e)=>setSurname(e.target.value)} 
+                    type="text" 
+                    holder="Ingresa tu Apellido" 
+                    divclass="mb-3" 
+                />
+                <InputForm 
+                    value={dni} 
+                    onChange={(e)=>setDni(e.target.value)} 
+                    type="number" 
+                    holder="Ingresa tu DNI" 
+                    divclass="mb-3" 
+                />
+                <InputForm 
+                    value={phone} 
+                    onChange={(e)=>setPhone(e.target.value)} 
+                    type="tel" 
+                    holder="Ingresa tu telefono" 
+                    divclass="mb-3" 
+                />
+                <InputForm 
+                    value={email} 
+                    onChange={(e)=>setEmail(e.target.value)} 
+                    type="email" 
+                    holder="Ingresa tu Email" 
+                    divclass="mb-3" 
+                />
+                <InputForm 
+                    value={password} 
+                    onChange={(e)=>setPassword(e.target.value)} 
+                    type="password" 
+                    holder="Ingresa una contraseña" 
+                    divclass="mb-3 "
+                />
+
 
                     <ButtonInput type="submit" text="Registrarse" divclass="mt-6" />
                 </form>
