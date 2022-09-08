1922413b07409b3e6314a4e5f680bbb268f909ad
santiagoavilez <santiagolingreed@gmail.com>
2022-09-08 16:28:51 -0300

    puerto 80 modificaccion

--- back-larevel/config/sanctum.php
+++ back-larevel/config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s%s',
-        'https://uncoactiva.fi.uncoma.edu.ar/,https://uncoactiva-back.fi.uncoma.edu.ar,::80',
+        'https://uncoactiva.fi.uncoma.edu.ar/,https://uncoactiva.fi.uncoma.edu.ar/,https://uncoactiva-back.fi.uncoma.edu.ar,::80',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : '',
         env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
     ))),
