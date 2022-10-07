c68eb00303349535f4298824dba3b1dad3efad18
cristian96-code <cristian.garrado@est.fi.uncoma.edu.ar>
2022-10-06 23:02:50 -0300

    view regulacion in progress

--- front-react/src/pages/Regulation.jsx
+++ front-react/src/pages/Regulation.jsx
@@ -5,39 +5,39 @@ const regulation = (/* categories, setcategories */) => {
   return (
     <AppLayout>
       <div className="font-Hurme-Geometric-R min-h-screen">
-        <div className="px-3 md:px-20 py-7 ">
+        <div className="px-3 sm:px-10 md:px-20 lg:px-32 py-7 ">
           <div className=' bg-gray-light rounded-lg text-black'>
-            <div className='py-5 '>
-              <div className='grid justify-center px-5 sm:px-5 lg:px-16 '>
+            <div className='py-10'>
+              <div className='grid justify-center px-5 sm:px-5 lg:px-16 py-5'>
                 <p className='text-blue-dark text-3xl sm:text-4xl font-bold font-Hurme-Geometric-BO'>REGLAMENTO</p>
                 <h1 className=' bg-yellow py-1 w-1/3 mt-5'> </h1>
               </div>
-              <div className="px-5 lg:px-16">
+              <div className="px-3 sm:px-5 lg:px-12">
                 <div className="sm:px-5">
                   <h4 className="text-[1.2rem] text-blue-high font-bold pt-10 font-Hurme-Geometric-BO">REGLAS GENERALES PARA LOS PARTICIPANTES</h4>
-                  <p className='font-Hurme-Geometric-N'>Los y las participantes deberan:</p>
-                  <ul className='list-decimal pl-5 font-Hurme-Geometric-N'>
-                    <li>Es responsabilidad del participante estar bien preparado para la prueba. Esto es, gozar de buena salud en general,  así como tener un nivel aceptable de preparación. La inscripción en una prueba no asegura ni cubre esta responsabilidad.</li>
-                    <li>La organización recomienda que todos los participantes se realicen un control médico previo a la carrera para asegurarse estar apto para el evento.</li>
-                    <li>Es obligatorio completar la ficha de inscripción publicada en forma on line en el sitio oficial de la carrera.</li>
-                    <li>La organización, a través de los fiscales autorizados, se reserva el derecho de interrumpir la participación de aquellos competidores que por su condición se considere que están poniendo en riesgo su integridad física, en caso de ser indicado, es obligación del participante hacer caso a la misma. De no respetarse, será descalificado y la responsabilidad corre por cuenta del propio participante.</li>
-                    <li>La organización dispondrá ambulancias y enfermeros para la asistencia médica extra hospitalaria a quienes lo necesiten. Cada corredor está en conocimiento de las posibles consecuencias de la práctica de una actividad de este tipo, deberá por lo tanto asumir y ser responsable de cualquier gasto relacionado a emergencias médicas, salvo aquellos cubiertos por el seguro de corredor y la atención de la emergencia en terreno ya sea con la ambulancia, socorristas, enfermeros o médicos de la organización de la carrera. En caso de ser necesario, para continuar con los primeros auxilios, serán trasladados al hospital más cercano. Es fundamental que cada corredor al completar la ficha de inscripción detalle en “Observaciones” los datos de su cobertura médica y un teléfono de urgencia.</li>
-                    <li>El participante tiene la obligación de conocer y respetar estas reglas de competición, así como las normas de circulación y las instrucciones de los responsables de cada prueba.</li>
-                    <li>El participante tiene la obligación de conocer, defender y respetar el medio ambiente en el que se realiza el evento. La participación en la prueba no exime al participante de esta obligación. El mal trato o la falta de respeto hacia el medio (barda) puede ser motivo de descalificación, pudiendo llegarse a la expulsión de la competición general.</li>
-                    <li>El participante que abandona la competición está obligado a quitarse el número y comunicar a los jueces, fiscales o banderilleros su abandono, entregando el troquel del respectivo número.</li>
-                    <li>En la charla técnica, previa a largada, se brindará una explicación sobre el circuito del recorrido. En caso de que un participante se extravíe durante la competencia, por no ver una cinta, o no prestar atención a los senderos o indicaciones por parte de la organización, los corredores son responsables de volver al camino y retomar el recorrido. Ante cualquier reclamo por extravío la organización no se hace responsable.</li>
-                    <li>El recorrido podrá ser modificado antes o incluso durante el desarrollo mismo de la carrera atendiendo a razones de seguridad de los participantes u otras circunstancias que a consideración de la organización lo demanden. En caso de que la decisión sea tomada antes de la largada, los corredores serán notificados. En caso de que la decisión sea tomada durante el desarrollo de la carrera, los participantes serán informados en el puesto de control anterior a la modificación. La organización no se hará responsable de cualquier tipo de reclamo por parte de los participantes por esta causa.</li>
-                    <li>Las imágenes que se obtengan en la competencia podrán ser utilizadas por la organización y por las empresas auspiciantes para fines de difusión y publicidad del evento y/o de productos asociados al mismo.</li>
-                    <li>Los competidores que acepten participar en este evento no tendrán derecho a realizar reclamos.</li>
-                    <li>Responsabilidades: Al inscribirse, el participante acepta el presente reglamento y declara:
+                  <p className='font-Hurme-Geometric-N pt-2'>Los y las participantes deberan:</p>
+                  <ul className='list-decimal pl-5 font-Hurme-Geometric-Nd'>
+                    <li >Es responsabilidad del participante estar bien preparado para la prueba. Esto es, gozar de buena salud en general,  así como tener un nivel aceptable de preparación. La inscripción en una prueba no asegura ni cubre esta responsabilidad.</li>
+                    <li className='pt-5'>La organización recomienda que todos los participantes se realicen un control médico previo a la carrera para asegurarse estar apto para el evento.</li>
+                    <li className='pt-5'>Es obligatorio completar la ficha de inscripción publicada en forma on line en el sitio oficial de la carrera.</li>
+                    <li className='pt-5'>La organización, a través de los fiscales autorizados, se reserva el derecho de interrumpir la participación de aquellos competidores que por su condición se considere que están poniendo en riesgo su integridad física, en caso de ser indicado, es obligación del participante hacer caso a la misma. De no respetarse, será descalificado y la responsabilidad corre por cuenta del propio participante.</li>
+                    <li className='pt-5'>La organización dispondrá ambulancias y enfermeros para la asistencia médica extra hospitalaria a quienes lo necesiten. Cada corredor está en conocimiento de las posibles consecuencias de la práctica de una actividad de este tipo, deberá por lo tanto asumir y ser responsable de cualquier gasto relacionado a emergencias médicas, salvo aquellos cubiertos por el seguro de corredor y la atención de la emergencia en terreno ya sea con la ambulancia, socorristas, enfermeros o médicos de la organización de la carrera. En caso de ser necesario, para continuar con los primeros auxilios, serán trasladados al hospital más cercano. Es fundamental que cada corredor al completar la ficha de inscripción detalle en “Observaciones” los datos de su cobertura médica y un teléfono de urgencia.</li>
+                    <li className='pt-5'>El participante tiene la obligación de conocer y respetar estas reglas de competición, así como las normas de circulación y las instrucciones de los responsables de cada prueba.</li>
+                    <li className='pt-5'>El participante tiene la obligación de conocer, defender y respetar el medio ambiente en el que se realiza el evento. La participación en la prueba no exime al participante de esta obligación. El mal trato o la falta de respeto hacia el medio (barda) puede ser motivo de descalificación, pudiendo llegarse a la expulsión de la competición general.</li>
+                    <li className='pt-5'>El participante que abandona la competición está obligado a quitarse el número y comunicar a los jueces, fiscales o banderilleros su abandono, entregando el troquel del respectivo número.</li>
+                    <li className='pt-5'>En la charla técnica, previa a largada, se brindará una explicación sobre el circuito del recorrido. En caso de que un participante se extravíe durante la competencia, por no ver una cinta, o no prestar atención a los senderos o indicaciones por parte de la organización, los corredores son responsables de volver al camino y retomar el recorrido. Ante cualquier reclamo por extravío la organización no se hace responsable.</li>
+                    <li className='pt-5'>El recorrido podrá ser modificado antes o incluso durante el desarrollo mismo de la carrera atendiendo a razones de seguridad de los participantes u otras circunstancias que a consideración de la organización lo demanden. En caso de que la decisión sea tomada antes de la largada, los corredores serán notificados. En caso de que la decisión sea tomada durante el desarrollo de la carrera, los participantes serán informados en el puesto de control anterior a la modificación. La organización no se hará responsable de cualquier tipo de reclamo por parte de los participantes por esta causa.</li>
+                    <li className='pt-5'>Las imágenes que se obtengan en la competencia podrán ser utilizadas por la organización y por las empresas auspiciantes para fines de difusión y publicidad del evento y/o de productos asociados al mismo.</li>
+                    <li className='pt-5'>Los competidores que acepten participar en este evento no tendrán derecho a realizar reclamos.</li>
+                    <li className='pt-5'>Responsabilidades: Al inscribirse, el participante acepta el presente reglamento y declara:
                       <ul className='pl-5'>
                         <li>a) Saber que habrá lugares a los que no se puede acceder con vehículos, con lo cual la atención médica inmediata es limitada.</li>
                         <li>b) Conocer las características del terreno y los riesgos posibles de lesiones traumatológicas como torceduras, esguinces, incluso fracturas y, que por razones de seguridad, la organización priorizará la atención y evacuación de las emergencias y urgencias médicas, considerándose como tales a aquellos casos que puedan evolucionar en riesgo de muerte, pudiendo demandar más tiempo que la atención de lesiones como las antes descriptas.</li>
                         <li>c) Eximir a los Organizadores, Municipios, Propietarios de las tierras del circuito de la carrera y los Patrocinantes de toda responsabilidad por accidentes personales; daños y/o pérdidas de objetos que pudiera ocurrirle antes, durante o después de su participación en la prueba.</li>
                       </ul>
                     </li>
-                    <li>Modo de inscripción: El participante deberá inscribirse a la carrera por la web  <a href='https://uncoactiva.fi.uncoma.edu.ar/' target='_blank' rel='noreferrer' className='font-Hurme-Geometric-BO text-blue-cyan'>Unco Activa</a>, realizando el pago, únicamente por transferencia a la siguiente cuenta bancaria:
-                      <ul className='list-none py-3 pl-5'>
+                    <li className='pt-5'>Modo de inscripción: El participante deberá inscribirse a la carrera por la web  <a href='https://uncoactiva.fi.uncoma.edu.ar/' target='_blank' rel='noreferrer' className='font-Hurme-Geometric-BO text-blue-cyan'>Unco Activa</a>, realizando el pago, únicamente por transferencia a la siguiente cuenta bancaria:
+                      <ul className='list-none pt-3 pl-5'>
                         <li className='font-Hurme-Geometric-N'>Banco Credicoop Cooperativo Limitado</li>
                         <li><span className='font-Hurme-Geometric-N'>Adherente:</span> Universidad Nacional del Comahue.</li>
                         <li><span className='font-Hurme-Geometric-N'>Operador:</span> 549505 Roberto Antonio Sepulveda.</li>
@@ -45,7 +45,7 @@ const regulation = (/* categories, setcategories */) => {
                         <li><span className='font-Hurme-Geometric-N'>CBU:</span> 19100933-55009302490896</li>
                       </ul>
                     </li>
-                    <li>
+                    <li className='pt-5'>
                       <p className='font-Hurme-Geometric-BO'>Acreditaciones: <span className='font-Hurme-Geometric-N'>El participante deberá asistir a las acreditaciones, los días:</span></p>
                       <ul className='list-decimal pl-5 font-Hurme-Geometric-N'>
                         <li>Jueves 20 de oct de 18 a 21hs.</li>
@@ -55,9 +55,16 @@ const regulation = (/* categories, setcategories */) => {
                     </li>
                   </ul>
 
+                  <h4 className="text-[1.2rem] text-blue-high font-bold pt-5 font-Hurme-Geometric-BO">Largada y llegada</h4>
+                  <p className="font-Hurme-Geometric-N">Estacionamiento Central – Neuquén Capital.</p>
+                  <p className="font-Hurme-Geometric-N">Hora: 11hs</p>
                   <h4 className="text-[1.2rem] text-blue-high font-bold pt-5 font-Hurme-Geometric-BO">Quien participa</h4>
                   <p className="">Toda persona que cumpla los requisitos de la inscripción. La modalidad de dicha carrera se va a dar en fomar individual.</p>
-                  <h4 className="text-[1.2rem] text-blue-high font-bold pt-5 font-Hurme-Geometric-BO">¿Como ganar?</h4>
+                  <h4 className="text-[1.2rem] text-blue-high font-bold pt-5 font-Hurme-Geometric-BO">Disciplina</h4>
+                  <p className="">Trail de 3K, 7K, 15K y 25K.</p>
+                  <h4 className="text-[1.2rem] text-blue-high font-bold pt-5 font-Hurme-Geometric-BO">Recorrido</h4>
+                  <p className="">Tanto el recorrido de los 25km como el de 15km, será por senderos de barda y calles.  El recorrido de los 7km será por barda pero de dificultad baja. Y por último, los 3km de la caminata será por senderos accesibles casi sin desnivel.</p>
+                  <h4 className="text-[1.2rem] text-blue-high font-bold pt-5 font-Hurme-Geometric-BO">Formato de Calificación</h4>
                   <p>Los y las participantes deberan:</p>
                   <ul className='list-decimal pl-5'>
                     <li>Realizar el recorrido en el menor tiempo posible.</li>
@@ -74,7 +81,7 @@ const regulation = (/* categories, setcategories */) => {
                   <p>La remera de corredor será de uso obligatorio con el número abrochado en el frente de la misma. Los participantes deberán hacer uso del chip, en el caso de que se utilice como sistema de clasificación.</p>
                 </div>
 
-                <div className="sm:px-5">
+                {/* <div className="sm:px-5">
                   <h4 className="text-[1.2rem] text-blue-high font-bold pt-5 font-Hurme-Geometric-BO">Inscripciones</h4>
                   <p>El costo de la inscripción para cada distancia es el siguiente:</p>
                   <ul className='list-disc pl-5'>
@@ -108,9 +115,9 @@ const regulation = (/* categories, setcategories */) => {
                   </ul>
                   <p className='text-blue-high text-[1.2rem] font-Hurme-Geometric-BO'>Una vez hecha la transferencia se debera adjuntar en el formulario de inscripcion, en caso de tener algun inconveniente comunicarse a nuestro <a className='text-yellow' href='mailto:uncoactiva@gmail.com'> email </a> 
                   </p>
-                </div>
+                </div> */}
               </div>
-              <div className='px-5 sm:px-5 lg:px-16 pt-10'>
+              {/* <div className='px-5 sm:px-5 lg:px-16 pt-10'>
                 <p className='text-blue-dark text-3xl sm:text-4xl font-bold font-Hurme-Geometric-BO'>Premiación categorias de los 25 , 15 y 7 KM:</p>
                 <h1 className=' bg-yellow py-1 w-1/6 mt-5'> </h1>
               </div>
@@ -128,7 +135,7 @@ const regulation = (/* categories, setcategories */) => {
                     <li>+ 60 años</li>
                   </ul>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
