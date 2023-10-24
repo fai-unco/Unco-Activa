4befeb9aa441ae019de8b747a6b631d7c722bca2
Frlawer <frlawer@gmail.com>
2023-10-24 14:47:54 -0300

    add email alert to change race date

---
+++ back-larevel/resources/views/mail/alerta-mail.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Información importante.
+    ---
+
+### Informamos mediante este mensaje que habida cuenta del alerta meteorológico por lluvias y ráfagas de vientos de hasta 120 km/h que se preveen para este viernes y sábado, la carrera se pospone para el mismo Domingo 29 del corriente a las 9 AM.
+
+## Recordamos que las acreditaciones se otorgarán el día Sábado de 12 a 16hs, en caso de situaciones de fuerza mayor se dara acreditación antes de la carrera, teniendo en cuenta el no llegar a tramitar la acreditación sobre el horario de largada, pues se respetará el horario de manera puntual. 
+
+## Si tienes algun inconveniente, no dudes en comunicarte con nosotros, a traves de este medio.
+## Visita la web https://uncoactiva.fi.uncoma.edu.ar/ para más información.
+    
+@endcomponent
