47d5083cf380df47f0dda987dba45ea07c60ad1d
Frlawer <frlawer@gmail.com>
2023-10-24 16:52:55 -0300

    add link to send emails, add EmailControlller

---
+++ back-larevel/app/Http/Controllers/EmailController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support;
+use App\Mail\AlertMail;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+
+class EmailController extends Controller
+{
+    public function sendEmails(){
+        $emails = DB::table('inscriptions')->pluck('email');
+
+        foreach ($emails as $emails) {
+            Mail::to($emails)->send(new AlertMail());
+        }
+        return redirect()->back()->with('success', 'Emails enviados correctamente');
+    }
+}
