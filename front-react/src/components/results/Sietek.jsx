3a214be826334a403274d08b3e3069c19439a5e3
Frlawer <frlawer@gmail.com>
2023-10-29 20:33:01 -0300

    add component to view table html files

---
+++ front-react/src/components/results/Sietek.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Sietek = () => {
+    return (
+        <>
+            <iframe
+                title="7 K"
+                src="sietek.html"
+                width="100%"
+                height="600px"
+            />
+        </>
+    );
+};
+
+export default Sietek;
---
+++ front-react/src/pages/Results.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Quincek from 'components/results/Quincek';
+import Sietek from 'components/results/Sietek';
+
+const Results = () => {
+    return (
+        <>
+            <Quincek />
+            <hr />
+            <Sietek />
+        </>
+    );
+};
+
+export default Results;
