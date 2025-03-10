43c6efe07d4e8fcb821b89e67c1bb3552b418e1e
LeonardoAluheCorrea <leonardo.correa@est.fi.uncoma.edu.ar>
2025-03-09 22:56:21 -0300

    Se modifico la condicion para permitir inscripciones. En lugar de revisar los cupos (quotas) disponibles en la categoria correspondiente, vemos que queden cupos disponibles en cualquiera, ya que segun los requisitos en el 2025 no hay cupos por categoria, solo un cupo total de 600 participantes

--- front-react/src/App.jsx
+++ front-react/src/App.jsx
@@ -10,10 +10,17 @@ import { CategorieContext } from 'context/CategorieContext';
 import Inscriptions from 'pages/Inscriptions';
 import AboutUs from 'pages/AboutUs';
 import Results from 'pages/Results';
+
+
 function App() {
   let categories = useContext(CategorieContext)
   categories = categories[0]  
-  
+
+
+
+const totalCupos = categories.reduce((sum, cat) => sum + cat.quotas, 0);
+
+
   return (
     <div id='top' className="antialiased">
       <Routes>
@@ -24,13 +31,28 @@ function App() {
         <Route element={<Results />} path='/resultados' />
         <Route element={<AboutUs />} path='/sobrenosotros' />
         <Route element={<Preinscription  />} path='/inscribirse' />
-        {categories.map( (categorie) =>  (          
-          categorie.quotas > 0 ? 
+         
+        {/* Mostrar pag de inscripcion solo si la suma total de cupos disponibles es > 0*/}
+      {totalCupos > 0 &&
+          categories.map((categorie) => (
             <Route
               key={categorie.id}
+              path={`/inscribirse/${categorie.name}`}
               element={<PreinscriptionForm categorie={categorie} />}
-              path={'/inscribirse/' + categorie.name} /> : <></>      
-        ))}
+            />
+          ))
+        }
+
+        {/* Esto es si tenemos un cupo para cada categoria
+        {categories.map((categorie) =>
+        categorie.quotas > 0 ? (
+          <Route
+            key={categorie.id}
+            path={`/inscribirse/${categorie.name}`}
+            element={<PreinscriptionForm categorie={categorie} />}
+          />
+        ) : null
+      )}*/}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
