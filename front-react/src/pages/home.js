c78143e3076dc0bdd816821a63c05659d5634447
LeonardoAluheCorrea <leonardo.correa@est.fi.uncoma.edu.ar>
2025-03-09 22:50:03 -0300

    Se actualizo la info a la del 2025 y se modifico ligeramente la colocacion de los textos en la pagina principal

--- front-react/src/pages/home.js
+++ front-react/src/pages/home.js
@@ -12,7 +12,7 @@ import { Fab } from "@mui/material";
 import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
 
 const mockImagenes = [
-    "elements/camiseta-2023.jpg",
+    //"elements/camiseta-2023.jpg",
     "elements/unco-activa2023.jpeg",
     "elements/edicion-anterior1.jpeg",
     "elements/edicion-anterior2.jpeg",
@@ -72,104 +72,98 @@ const Home = () => {
             <div className="px-3 md:px-12 py-7  ">
                 <div className=" bg-gray-light rounded-lg text-black">
                     <div id="conoceMas" className="text-[1.8rem] text-center p-5 font-Hurme-Geometric-BO text-red-600">
-                        <h1 className="">DOMINGO 29 OCT / 09:00 HRS</h1>
+                        <h1 className="">SÁBADO 26 ABRIL / 09:00 HRS</h1>
                         <h1>Universidad Nacional del Comahue - Neuquén Capital</h1>
                     </div>
                     <div className="flex justify-center pt-5">
                         <Slider imagenes={mockImagenes} className="bg-white" />
                     </div>
 
-                    <div className="py-5 ">
-                        <div className="px-5 sm:px-5 lg:px-16">
-                            <p className="text-blue-dark text-3xl sm:text-4xl font-bold font-Hurme-Geometric-BO">Requisitos de la carrera</p>
-                            <h1 className=" bg-yellow py-1 w-1/6 mt-5"> </h1>
-                        </div>
-                        <div className="grid sm:grid-cols-2 px-5 lg:px-16">
-                            <div className="sm:px-5">
-                                <h4 className="text-[1.2rem] text-blue-high font-bold py-5 font-Hurme-Geometric-BO">Quién participa</h4>
-                                <p className="">Toda persona que cumpla los requisitos de la inscripción. La modalidad de dicha carrera se va a dar en forma individual.</p>
-                                <h4 className="text-[1.2rem] text-blue-high font-bold py-5 font-Hurme-Geometric-BO">¿Cómo ganar?</h4>
-                                <p>Los y las participantes deberán:</p>
-                                <ul className="list-decimal pl-5">
-                                    <li>Realizar el recorrido en el menor tiempo posible.</li>
-                                    <li>No podrán utilizar vehículos ni acortar el recorrido.</li>
-                                    <li>No se puede ser asistido por otras personas en competencia (fair play).</li>
-                                    <li>La largada será común para todas las distancias. Deberán seguir las marcas.</li>
-                                </ul>
-                                <h4 className="text-[1.2rem] text-blue-high font-bold py-5 font-Hurme-Geometric-BO">Servicios, Asistencia, Hidratación y Controles</h4>
-                                <ul className="list-decimal pl-5">
-                                    <li>En un punto intermedio del recorrido y en la llegada se montarán puestos de asistencia e hidratación.</li>
-                                    <li>A lo largo del recorrido se establecerán controles de paso de los corredores y las corredoras en donde habrá colaboradores con remeras.</li>
-                                </ul>
-                                <h4 className="text-[1.2rem] text-blue-high font-bold py-5 font-Hurme-Geometric-BO">Remera de competencia y número de identificación de corredor</h4>
-                                <p>La remera de corredor será de uso obligatorio con el número abrochado en el frente de la misma. Los participantes deberán hacer uso del chip, en el caso de que se utilice como sistema de clasificación.</p>
-                            </div>
+                    <div className="py-5">
+    <div className="px-5 sm:px-5 lg:px-16">
+        <h1 className="bg-yellow py-1 w-1/6 mt-5"></h1>
+    </div>
 
-                            <div className="sm:px-5">
-                                <h4 className="text-[1.2rem] text-blue-high font-bold py-5 font-Hurme-Geometric-BO">Acreditaciones: </h4>
-                                <p>
-                                    El participante deberá asistir a las acreditaciones, el día: <span className="text-[1.2rem] text-blue-high font-bold py-5 font-Hurme-Geometric-N">Sábado 28 de Octubre de 12 a 16 hs.</span> <strong className="text-rose-500 font-extrabold text-[1.4em] font-Hurme-Geometric-BO">Lugar: Gimnasio polideportivo Roberto Monteros (Escondido) predio de la UNCO - Neuquén Capital!</strong>
-                                </p>
-                                <p>La remera de corredor será de uso obligatorio con el número abrochado en el frente de la misma. Los participantes deberán hacer uso del chip, en el caso de que se utilice como sistema de clasificación.</p>
-                                <p>Para retirar su remera, número y chip, y firmar el deslinde de responsabilidad.</p>
-                                <h4 className="text-[1.2rem] text-blue-high font-bold pt-5 font-Hurme-Geometric-BO">Inscripciones</h4>
-                                <p>El costo de la inscripción para cada distancia es el siguiente:</p>
-                                <ul className="list-disc pl-5">
-                                    <p className="font-bold">Hasta el 30 de Septiembre (o hasta cubrir el cupo):</p>
-                                    <li>15 km tiene un costo de $6800 por participante.</li>
-                                    <li>7 km tiene un costo de $4800 por participante</li>
-                                    <li>3 km tiene un costo de $2800 por participante. Es sin clasificación ni premiación.</li>
-                                    <br></br>
-                                    <p className="font-bold">A partir del 1 de Octubre:</p>
-                                    <li>15 km tiene un costo de $7800 por participante.</li>
-                                    <li>7 km tiene un costo de $5800 por participante</li>
-                                    <li>3 km tiene un costo de $3800 por participante. </li>
-                                </ul>
+        <div className="px-5 lg:px-16">
+            <p className="text-blue-dark text-lg sm:text-xl font-bold font-Hurme-Geometric-BO">
+                La carrera UNCo Activa surge en el año 2017 y se ha convertido en un evento emblemático dentro de la comunidad de la Universidad Nacional del Comahue, destacando no solo por su carácter competitivo, sino también por su enfoque en la salud y el bienestar de todos los participantes. Esta iniciativa refleja el compromiso de la Universidad con la promoción de un estilo de vida activo y saludable, alineándose con los objetivos del área de deportes y recreación de nuestra Institución.
+            </p>
 
-                                <p className="pt-2">
-                                    <span className="font-Hurme-Geometric-N">Incluye: </span>
-                                    Derecho a participación / Remera técnica / Servicios de control / Servicio de hidratación seguro de corredor.
-                                </p>
+            <p className="mt-4">
+                Gran parte del recorrido se realiza en el Área Protegida Parque Universitario Provincia del Monte. Este parque fue creado mediante la ordenanza Nº 714/2004 del Consejo Superior de la Universidad Nacional del Comahue. Ocupa un área de 70 ha en la parte norte de los terrenos del campus de nuestra institución en la ciudad de Neuquén.
+            </p>
 
-                                <div className="flex xl:pt-8 xl:pb-1 justify-center xl:my-0 my-5 px-2 -z-0 relative">
-                                    <Fab color="primary" variant="extended" size="small" href={"/inscribirse"}>
-                                        <LibraryBooksOutlinedIcon className="mr-3" /> Ir a Inscripciones
-                                    </Fab>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="px-5 sm:px-5 lg:px-16 pt-10">
-                            <p className="text-blue-dark text-3xl sm:text-4xl font-bold font-Hurme-Geometric-BO">Premiación categorías de los 15 y 7 KM:</p>
-                            <h1 className=" bg-yellow py-1 w-1/6 mt-5"> </h1>
-                        </div>
-                        <div className="grid sm:grid-cols-2 px-5 lg:px-16">
-                            <div className="sm:px-5">
-                                <p className="text-[1.2rem] text-blue-high pt-5 font-Hurme-Geometric-BO">Premiación por categoría por edades, tanto en la rama femenina y masculina</p>
-                                <ul className="list-disc pl-5 pt-2">
-                                    <li>Hasta 19 años</li>
-                                    <li>Hasta 29 años</li>
-                                    <li>Hasta 39 años</li>
-                                    <li>Hasta 49 años</li>
-                                    <li>Hasta 59 años</li>
-                                    <li>+ 60 años</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+            <p className="mt-4">
+                Participar en la carrera UNCo Activa no solo brinda la oportunidad de disfrutar de una jornada de ejercicio al aire libre, sino que también fomenta la integración y el compañerismo entre los miembros de la Comunidad Universitaria y la Comunidad en General. Es un espacio donde se celebra el esfuerzo personal y colectivo, promoviendo valores como la perseverancia, la disciplina y el trabajo en equipo.
+            </p>
+
+            <p className="mt-4">
+                Esta carrera se enmarca dentro de una serie de actividades deportivas que la Universidad organiza a lo largo del año, contribuyendo a la formación integral de los estudiantes en particular y el resto de la comunidad. Al incentivar la práctica del trote y el senderismo, se busca no solo mejorar la condición física, sino también reducir el estrés y mejorar la salud mental.
+            </p>
+
+            <p className="mt-4 font-bold text-blue-dark">
+                La carrera UNCo Activa es más que una competencia, es una celebración del deporte y la comunidad universitaria. Su relación con el área de deportes y recreación de la Universidad Nacional del Comahue resalta la importancia de fomentar hábitos saludables y crear un ambiente inclusivo donde todos puedan disfrutar de los beneficios del ejercicio.
+            </p>
+
+            <p className="mt-4 text-lg font-bold text-blue-dark">
+                ¡Así que a calzarse las zapatillas y a correr, trotar o caminar juntos por una vida activa, siempre en Defensa de la Universidad Pública!
+            </p>
+        </div>
+
+        <div className="px-5 sm:px-5 lg:px-16 pt-10">
+            <h4 className="text-[1.2rem] text-blue-high font-bold pt-5 font-Hurme-Geometric-BO">
+                Inscripciones - Categorias
+            </h4>
+
+            <ul className="list-disc pl-5">
+                <p className="font-bold">Montos de inscripción:</p>
+                <li>15 km $45000.</li>
+                <li>7 km $40000.</li>
+                <li>3 km $30000 (descuento 10% grupo familiar - solo 3 km).</li>
+                <br />
+                <p className="font-bold">Montos diferenciados para comunidad Universitaria:</p>
+                <li>15 km $42000.</li>
+                <li>7 km $37000.</li>
+                <li>3 km $25000(descuento del 10% por grupo familiar).</li>
+            </ul>
+
+            <p className="pt-2">
+                <span className="font-Hurme-Geometric-N">Incluye: </span>
+                Derecho a participación / Remera técnica / Servicios de control / Servicio de hidratación / Seguro de corredor.
+            </p>
+
+            <div className="flex xl:pt-8 xl:pb-1 justify-center xl:my-0 my-5 px-2 -z-0 relative">
+                <Fab color="primary" variant="extended" size="small" href={"/inscribirse"}>
+                    <LibraryBooksOutlinedIcon className="mr-3" /> Ir a Inscripciones
+                </Fab>
+            </div>
+        </div>
+    </div>
+
 
                     <div className="grid justify-center pb-5">
                         <h1 className="text-center text-[2rem]  md:text-[2.5rem] font-Hurme-Geometric-BO text-blue-dark" fill="currentColor">
                             ORGANIZADORES
                         </h1>
 
-                        <div id="racePath" className="grid grid-cols-5 gap-4 items-center justify-center p-5">
-                            <img className="col-span-1 h-24 md:h-[180px] justify-self-end" alt="Dirección de deportes y recreación" src={"/logos/Logo Dirección de Deportes y Recreación 4-10.png"} />
-                            <img className="col-span-1 h-24 md:h-[180px] justify-self-center " alt="Unco" src={"/logos/LOGO UNCO PNG.png"} />
-
-                            <img className="col-span-1 h-24 md:h-[180px] justify-self-start bg-gray-darker" alt="Secretaria de Bienestar Universitario" src={"/logos/Secretaria_de_bienestar_universitario.png"} />
-                            <img className="col-span-1 h-24 md:h-[180px] justify-self-start bg-gray-darker" alt="logo deportes" src={"/logos/logo_deporte.png"} />
-                            <img className="col-span-1 h-24 md:h-[180px] justify-self-start bg-gray-darker" alt="logo anaar" src={"/logos/Anaar.png"} />
+                        <div id="racePath" className="grid grid-cols-3 gap-4 items-center justify-center p-5">
+                        <img 
+                            className="col-span-1 h-24 md:h-[180px] justify-self-start" 
+                            alt="Dirección de deportes y recreación" 
+                            src={"/logos/Logo Dirección de Deportes y Recreación 4-10.png"} 
+                        />
+                        <img 
+                            className="col-span-1 h-24 md:h-[180px] justify-self-center" 
+                            alt="Unco" 
+                            src={"/logos/LOGO UNCO PNG.png"} 
+                        />
+                        <img 
+                            className="col-span-1 h-24 md:h-[180px] justify-self-end bg-gray-darker" 
+                            alt="Secretaria de Bienestar Universitario" 
+                            src={"/logos/Secretaria_de_bienestar_universitario.png"} 
+                        />
                         </div>
+
                         <div></div>
                     </div>
                 </div>
